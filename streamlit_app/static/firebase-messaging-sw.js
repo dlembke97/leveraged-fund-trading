f41d8e3c4246b641f801f2b8bb20374d7e137071
dlembke3 <dlembke3@gatech.edu>
2025-05-30 05:48:17 -0500

    fix js script

--- streamlit_app/static/firebase-messaging-sw.js
+++ streamlit_app/static/firebase-messaging-sw.js
@@ -3,11 +3,11 @@ importScripts('https://www.gstatic.com/firebasejs/9.x/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/9.x/firebase-messaging.js');
 
 firebase.initializeApp({
-  apiKey: '<API_KEY>',
-  authDomain: '<PROJECT_ID>.firebaseapp.com',
-  projectId: '<PROJECT_ID>',
-  messagingSenderId: '<SENDER_ID>',
-  appId: '<APP_ID>'
+  apiKey: 'AIzaSyATtW92Kmqasqo1AiWMntz-eM4NrqZ2JXw',
+  authDomain: 'leveraged-fund-trading-notify.firebaseapp.com',
+  projectId: 'leveraged-fund-trading-notify',
+  messagingSenderId: '832074947393',
+  appId: '1:832074947393:web:e882205f72dae2f4a19fc0'
 });
 const messaging = firebase.messaging();
 
